0ddf9341b979e20bbf9c70a3960fdf48ceee7ffc
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-06-24 01:17:15 -0500

    First layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,146 @@
 <template>
   <BaseLayout>
-  
+    <v-container class="fill-height" fluid>
+      <!-- vistas -->
+      <v-row align="center" justify="center">
+        <v-col cols="12" :xs="10" :md="6">
+          <v-text-field
+            filled
+            hide-details
+            append-icon="mdi-magnify"
+            label="Search songs..."
+            rounded
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- Responsive rows y cols -->
+    <!-- 
+    <v-subheader>Recently played</v-subheader>
+    <v-row>
+      <v-col cols="12" :xs="12" :md="6" :lg="4">
+        <v-card color="#385F73" dark>
+          <v-card-title class="headline">Unlimited music now</v-card-title>
+
+          <v-card-subtitle
+            >Listen to your favorite artists and albums whenever and wherever,
+            online and offline.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn small text>Listen Now</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col
+        v-for="(item, i) in items"
+        :key="i"
+        cols="12"
+        :xs="12"
+        :md="6"
+        :lg="4"
+      >
+        <v-card :color="item.color" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            </div>
+
+            <v-avatar class="ma-3" size="125" tile>
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-subheader>Songs for you</v-subheader>
+
+    <v-row>
+      <v-col cols="12" :xs="12" :md="6" :lg="4">
+        <v-card color="#385F73" dark>
+          <v-card-title class="headline">Unlimited music now</v-card-title>
+
+          <v-card-subtitle
+            >Listen to your favorite artists and albums whenever and wherever,
+            online and offline.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn small text>Listen Now</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col
+        v-for="(item, i) in items"
+        :key="i"
+        cols="12"
+        :xs="12"
+        :md="6"
+        :lg="4"
+      >
+        <v-card :color="item.color" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            </div>
+
+            <v-avatar class="ma-3" size="125" tile>
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-subheader>May be interested</v-subheader>
+
+    <v-row>
+      <v-col cols="12" :xs="12" :md="6" :lg="4">
+        <v-card color="#385F73" dark>
+          <v-card-title class="headline">Unlimited music now</v-card-title>
+
+          <v-card-subtitle
+            >Listen to your favorite artists and albums whenever and wherever,
+            online and offline.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn small text>Listen Now</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col
+        v-for="(item, i) in items"
+        :key="i"
+        cols="12"
+        :xs="12"
+        :md="6"
+        :lg="4"
+      >
+        <v-card :color="item.color" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            </div>
+
+            <v-avatar class="ma-3" size="125" tile>
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row> -->
   </BaseLayout>
 </template>
 
@@ -12,7 +152,29 @@ export default {
     BaseLayout
   },
   data: () => ({
-    //
-  })
+    items: [
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People'
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding'
+      }
+    ]
+  }),
+
+  methods: {
+    getImage () {
+      const min = 550
+      const max = 560
+
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+  }
 }
 </script>
--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,41 +1,127 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
-    </v-navigation-drawer>
+    <!-- barra de opciones o navegador horizontal -->
+    <v-app-bar color="teal" dark flat app>
+      <!-- <v-btn v-if="!isMobile" icon>
+        <v-icon @click="isOpen = !isOpen">
+          mdi-chevron-{{ isOpen ? 'left' : 'right' }}
+        </v-icon>
+      </v-btn> -->
+      <v-btn v-if="!isMobile" icon>
+        <v-icon>
+          mdi-chevron-left
+        </v-icon>
+      </v-btn>
+      <v-btn v-if="!isMobile" icon>
+        <v-icon>
+          mdi-chevron-right
+        </v-icon>
+      </v-btn>
 
-    <v-app-bar dense dark flat app>
-      <!-- -->
+      <template v-if="isMobile"
+        ><v-icon class="mx-4" large>
+          mdi-spotify
+        </v-icon>
+        <v-toolbar-title class="mr-12 align-center">
+          <span class="title">Spotify</span>
+        </v-toolbar-title>
+      </template>
+
+      <!-- <v-text-field
+        filled
+        rounded
+        dense
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down ml-10"
+      ></v-text-field> -->
+
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+
+      <v-btn icon large>
+        <v-avatar size="32px" item
+          ><img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+        </v-avatar>
+      </v-btn>
     </v-app-bar>
 
-    <!-- Sizes your content based upon application components -->
+    <!-- menu en escritorio -->
+    <v-navigation-drawer permanent v-model="isOpen" v-if="!isMobile" app>
+      <v-list dense>
+        <v-list-item v-if="!isMobile">
+          <v-icon class="mx-4" large>
+            mdi-spotify
+          </v-icon>
+          <v-toolbar-title class="mr-12 align-center">
+            <span class="title">Spotify</span>
+          </v-toolbar-title>
+        </v-list-item>
+        <v-list-item v-for="item in items" :key="item.text" link>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-4 grey--text text--darken-1 text-uppercase"
+          >Popular</v-subheader
+        >
+        <v-list>
+          <v-list-item v-for="item in items2" :key="item.text" link>
+            <v-list-item-avatar>
+              <img
+                :src="
+                  `https://randomuser.me/api/portraits/men/${item.picture}.jpg`
+                "
+                alt=""
+              />
+            </v-list-item-avatar>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <v-list-item class="mt-4" link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1"
+            >Browse Channels</v-list-item-title
+          >
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1"
+            >Manage Subscriptions</v-list-item-title
+          >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Contenido de la aplicación -->
     <v-main>
       <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <slot />
-      </v-container>
+      <!-- vistas -->
+      <slot />
     </v-main>
 
-    <v-bottom-navigation v-model="bottomNav" v-if="isMobile" app>
-      <v-btn value="recent">
-        <span>Recent</span>
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-
-      <v-btn value="favorites">
-        <span>Favorites</span>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn value="nearby">
-        <span>Nearby</span>
-        <v-icon>mdi-map-marker</v-icon>
+    <!-- Menu en dispositivos moviles -->
+    <v-bottom-navigation v-model="bottomNav" color="teal" v-if="isMobile" app>
+      <v-btn v-for="item in items" :key="item.icon" :value="item.value">
+        <span>{{ item.text }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
       </v-btn>
     </v-bottom-navigation>
 
-    <v-footer app>
-      <!-- -->
-    </v-footer>
+    <!-- <v-footer app>
+    </v-footer> -->
   </v-app>
 </template>
 
@@ -43,8 +129,21 @@
 export default {
   data () {
     return {
-      bottomNav: 'favorites',
-      isMobile: false
+      bottomNav: 'search',
+      isMobile: false,
+      isOpen: false,
+      items: [
+        { icon: 'mdi-home', value: 'home', text: 'Home' },
+        { icon: 'mdi-magnify', value: 'search', text: 'Search' },
+        { icon: 'mdi-heart', value: 'fav', text: 'Favorites' }
+      ],
+      items2: [
+        { picture: 28, text: 'Joseph' },
+        { picture: 38, text: 'Apple' },
+        { picture: 48, text: 'Xbox Ahoy' },
+        { picture: 58, text: 'Nokia' },
+        { picture: 78, text: 'MKBHD' }
+      ]
     }
   },
   beforeDestroy () {
@@ -57,7 +156,7 @@ export default {
   },
   methods: {
     onResize () {
-      this.isMobile = window.innerWidth < 680
+      this.isMobile = window.innerWidth < 960
     }
   }
 }
